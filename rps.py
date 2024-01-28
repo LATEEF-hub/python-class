8ae9b571092adf51d356b3c7b91e49430ad872f9
LATEEF-hub <latifkk@krok.edu.ua>
2024-01-28 20:08:42 +0000

    clear comment and rearrage code

--- rps.py
+++ rps.py
@@ -3,13 +3,15 @@ import sys
 
 from enum import Enum
 
+
 class RPS(Enum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
 
+
 print("")
-print("****************") #header
+print("****************")  # header
 print("*   WELCOME!!! *")
 print("*      TO      *")
 print("*   Rock Paper *")
@@ -17,17 +19,18 @@ print("*   Scissors   *")
 print("****************")
 print("")
 
-playerchoice = input("Make a choice from 1-3 to start the game:\n\n1.Rock, \n2.Paper or \n3.Scissors\n")
-player = int(playerchoice) #Casting of player input
-#Validate player choice
+playerchoice = input(
+    "Make a choice from 1-3 to start the game:\n\n1.Rock, \n2.Paper or \n3.Scissors\n")
+player = int(playerchoice)  # Casting of player input
+# Validate player choice
 if player < 1 or player > 3:
     sys.exit("Invalid selection! Please enter either 1, 2 or 3.")
 
 computerchoice = random.choice("123")
-computer = int(computerchoice) #Casting of Computer input
+computer = int(computerchoice)  # Casting of Computer input
 
 
-#Result
+# Result
 print("")
 print("You choose " + str(RPS(player)).replace("RPS.", "") + ".")
 print("Python choose " + str(RPS(computer)).replace("RPS.", "") + ".")
@@ -43,21 +46,21 @@ elif playerchoice == 3 and computerchoice == 2:
 elif playerchoice == computerchoice:
     print("\nIt's a tie! Try Again.\n")
 else:
-    print("🐍 Python wins!")   
+    print("🐍 Python wins!")
+
+
+def playagain():
+    if input("Do you want to play again? (yes/no)\n").lower() == 'y':
+        return True
+    else:
+        return False
+        while playagain():
+            main()
+
+
+
 
 print("")
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # name = print(input("Enter your age:"))

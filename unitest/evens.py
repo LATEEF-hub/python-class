5b2548aba9008e0f99b1f51d6eff26e8802b23d5
LATEEF-hub <latifkk@krok.edu.ua>
2024-01-29 04:36:32 +0000

    added test for even num. check for create Testcase

--- unitest/evens.py
+++ unitest/evens.py
@@ -6,8 +6,22 @@ def even_num_of_evens(numbers):
     if the number of even numbers is 0, return False
     if the number of even numbers is even, return True
     """
+ #Method : Write a test that fails then add a code to the function that will make it pass
+    # using isinstance method to check 
+    # if the value entered is a list
     if isinstance(numbers, list):
-        return True
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False          
     else:
         raise TypeError("A list was not passed into the function, please check function!")
 
--- unitest/test_evens.py
+++ unitest/test_evens.py
@@ -2,8 +2,19 @@ import unittest
 from evens import even_num_of_evens
 
 class TestEvens(unittest.TestCase):
+    #Test to check for even number
     def test_throw_error_if_is_not_even(self):
-        self.assertRaises(TypeError, even_num_of_evens, 4)
+        self.assertRaises(TypeError, even_num_of_evens, )
+        
+    
+    def test_values_in_list(self):
+        #checking if the list entered is an empty list
+        self.assertEqual(even_num_of_evens([]), False)
+        #checking if the list entered is an 2 value in list
+        self.assertEqual(even_num_of_evens([4, 2]), True)
+        self.assertEqual(even_num_of_evens([8]), False)
+        self.assertEqual(even_num_of_evens([1,3,5]), False)
+
 
 
     # def test_function_returns_True(self):
